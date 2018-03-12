9a271a631a97fbed4a7b028ad1d520205c8a9a1d
Mitchell Koester <mkoester1999@hotmail.com>
2018-03-12 13:45:34 -0600

    fixed
    accidentally uploaded the wrong project.
    that one was an older one I tried to make in c++ but it didn't work very well

---
+++ Batch file maker csharp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+
+namespace Batch_file_maker_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = "hello.bat";
+            string path = Environment.ExpandEnvironmentVariables("C:\\users\\%username%\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\startup\\" + file);
+            StreamWriter writer = new StreamWriter(path);
+            writer.WriteLine("@echo off");
+            writer.WriteLine("echo you've been hacked bro");
+            writer.WriteLine("pause");
+            writer.Close();
+        }
+    }
+}
